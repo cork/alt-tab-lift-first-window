129bd9927ca8d702be4919babebc851341d2c4bd
Cork <cork@box.qzdev.se>
2014-09-29 22:56:27 +0200

    First version

---
+++ extension.js
@@ -0,0 +1,24 @@
+/* -*- mode: js; js-basic-offset: 4; indent-tabs-mode: nil -*- */
+
+const AltTab = imports.ui.altTab;
+
+let injections = {};
+
+function _select(app, window, forceAppFocus) {
+   return injections._select.call(this, app, window || 0, forceAppFocus);
+}
+
+function init(metadata) {
+}
+
+function enable() {
+   injections._select = AltTab.AppSwitcherPopup.prototype._select;
+   AltTab.AppSwitcherPopup.prototype._select = _select;
+}
+
+function disable() {
+   let prop;
+   for (prop in injections) {
+      AltTab.WindowSwitcherPopup.prototype[prop] = injections[prop];
+   }
+}
